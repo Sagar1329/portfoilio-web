a4178d9808a0b824d1ec226b87f4735a5e9eb918
Sagar1329 <sagargowda1329@gmail.com>
2025-07-26 21:32:54 +0530

    Added repo name

--- vite.config.js
+++ vite.config.js
@@ -1,6 +1,6 @@
 import { defineConfig } from 'vite';
 import react from '@vitejs/plugin-react';
 export default defineConfig({
-    base: "/YOUR_REPOSITORY_NAME/", // Replace with your repository name
+    base: "/portfoilio-web/", // Replace with your repository name
     plugins: [react()],
 });
